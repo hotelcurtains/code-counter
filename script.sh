63796cbae76b68334bc22c0330ff4c7bc9ffe21b
hotelcurtains <114700124+hotelcurtains@users.noreply.github.com>
2025-05-24 18:11:36 -0400

    major update: included all same-named files in final count. (wc ignored them previously, oops.) also moved to deleting dotfiles and git residue instead of just ignoring them.

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ collection="cc"
 
 # amount of repos you want to pull;
 # they are pulled in order of last commit.
-limit=50
+limit=3
 
 # script breaks if the collection folder exists.
 rm -rf ./"$collection"
@@ -17,23 +17,35 @@ rm -rf ./"$collection"
 # clone and flatten each repo
 gh repo list $username --limit $limit | while read -r repo _; do
 	gh repo clone "$repo" ./"$collection"/"$repo" -- --depth=1
-	rm -rf ./"$collection"/"$repo"/.git
-	find ./"$collection"/ -mindepth 2 -type f -name '*.*'  -exec mv -t ./"$collection"/ -f --backup='t' '{}' +
+
+	# removing .git info and dotfiles. they're hard to deal with and, really, who cares?
+	rm -rf ./"$collection"/"$repo"/.git*
+	find ./"$collection"/ -mindepth 2 -name ".*" -type f -exec rm -rf '{}' \;
+	
+	find ./"$collection"/ -mindepth 2 -name "*.*" -type f -exec mv '{}' ./"$collection" -f --backup='t' \;
 	rm -rf ./"$collection"/$username
 done
 
-echo -----------------------------------------
-
 # sort all the files
 for f in ./"$collection"/*; do
+	
+	if [[ "$f" == *.~* ]];then
+		old="$f"
+		f=$(echo $f | sed -r 's/(.*)\.(.*)\.~([0-9]{1,})~$/\1_\3.\2/')
+		mv -f "$old" "$f"
+	fi
+
 	f=$(basename "$f")
 	ext=$(echo ./"$collection"/"$(echo "${f%%.~*}" | cut -d '.' -f 2)"/ | tr '[:upper:]' '[:lower:]' )
-
-	( mkdir -p "$ext"  &&
+	
+	( ( mkdir -p "$ext" ) && 
 	mv -f "./$collection/$f" "${ext}" ) ||
-	( mkdir -p ./"$collection"/unknown/ &&
+	( (mkdir -p ./"$collection"/unknown/) &&
 	mv -f "./"$collection"/$f" ./"$collection"/unknown/ )
 done
+printf "\e[0m"
+
+echo -----------------------------------------
 
 # display line counts
 for f in ./"$collection"/*; do
